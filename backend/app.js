7e80641b79cde1c1671ff2168696806c64b4d321
Soumyabrata Sarkar <sarkar.soumyabrata2@gmail.com>
2024-08-31 13:51:51 +0530

    added docker file for backend

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use("/pathwise/api", appRouter);
 
 // Start the server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   appConfigInit();
   console.log(`Server is running on port ${PORT}`);
